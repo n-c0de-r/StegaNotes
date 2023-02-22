b7b6ecc8431a07cea9a945f22d446c5353865281
n-c0de-r <78597469+n-c0de-r@users.noreply.github.com>
2023-02-22 15:24:51 +0100

    separation of concerns

---
+++ Backend/source/app.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hello World");
+});
+
+export default app;
--- Backend/source/server.ts
+++ Backend/source/server.ts
@@ -1,14 +1,8 @@
-import "dotenv/config";
+import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
-import express from "express";
 
-const app = express();
 const port = env.PORT;
-
-app.get("/", (request, response) => {
-    response.send("Hello World");
-});
 
 mongoose.connect(env.MONGO_CONNECTION_STRING)
     .then(() => {
