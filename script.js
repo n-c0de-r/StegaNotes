2902f448d4551c0a377c75ba4643d581b4444291
n-c0de-r <78597469+n-c0de-r@users.noreply.github.com>
2023-03-21 23:12:42 +0100

    Basic login functionallity

--- script.js
+++ script.js
@@ -1,116 +1,471 @@
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker && navigator.serviceWorker.register("./sw.js");
-  });
+  navigator.serviceWorker.register("./sw.js");
 }
 
-loginContainer();
+let isValidUsername, isValidPassword, isConfirmed;
+
+loginHeader("abcd");
+// loginContainer();
+// loginFooter();
+
+// Content functions //
+
+function loginHeader(name) {
+  let head = `<img src="./icons/icon.svg" class="icon" alt="StegaNotes Icon">
+  <img src="./icons/name.svg" class="name" alt="StegaNotes Name">`;
+
+  if(name !== undefined) {
+    const svg = createSVG(name);
+    const url = serializeSVG(svg);
+
+    head = `
+      <img src=${url} style="width: 90%; float: left;" alt="User's Name">
+      <input type="checkbox" class="hamburger-input burger-shower" />
+			<label class="hamburger-menu" for="hamburger-input">
+				<nav class="sidebar-menu">
+				<h3>Menu</h3>
+				<ul>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				</nav>
+			</label>
+    `;
+  }
+
+  const header = document.querySelector(".header-content");
+  header.innerHTML = `${head}`;
+}
 
 function loginContainer() {
-  let container = document.querySelector("#container");
-  let loginFormHTML = `
+  const container = document.querySelector("section");
+  container.innerHTML = `
+    <div class="container">
       <form id="login-form">
         <div class="form-group">
-          <label for="username">Username</label>
-          <input type="text" id="username" name="username" required>
+          <label for="username" id="username-label">Username</label>
+          <input type="text" class="username" name="username" title="If the username is invalid, nothing will happen." placeholder="type here" required>
         </div>
         <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" id="password" name="password" required>
+          <label for="password" id="password-label">Password</label>
+          <input type="password" class="password" name="password" title="If the password is invalid, nothing will happen." placeholder="type here" required>
         </div>
         <div class="button-group">
-          <button type="submit" id="login-button">Login</button>
-          <button type="submit">Register</button>
+          <button type="submit" class="login-button">Login</button>
+          <button type="submit" class="register-button">Register</button>
         </div>
       </form>
-    `;
-  container.innerHTML = loginFormHTML;
+    </div>
+  `;
+
+  const loginButton = document.querySelector(".login-button");
+  loginButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    login();
+  });
+
+  const registerButton = document.querySelector(".register-button");
+  registerButton.addEventListener("click", registerContainer);
 }
 
-async function login(username, password) {
-  let objectStoreName = "credentials";
-  let db = await openDatabase(username, password);
-  let objectStore = db.transaction(objectStoreName, "readonly").objectStore(objectStoreName);
-  let request = objectStore.get(username);
+function registerContainer() {
+  const footer = document.querySelector("footer");
+  footer.classList.toggle('footer');
+  footer.innerHTML = "";
 
-  return new Promise((resolve, reject) => {
-    request.onsuccess = (event) => {
-      let user = event.target.result;
-      if (!user) {
-        reject("User not found");
+  const container = document.querySelector("section");
+  container.innerHTML = `
+    <div class="container">
+      <form id="login-form">
+        <div class="form-group">
+          <label for="username" id="username-label">New Username</label>
+          <input type="text" class="username" name="username" pattern="^.*(?=.{4,}).*$" title="The username must be at least 4 characters long." placeholder="min. 4 chars..." required>
+        </div>
+        <div class="form-group">
+          <label for="password" id="password-label">New Password</label>
+          <input type="password" class="password" name="password" pattern="^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?:{}|<>]).*$" title="The password must be at least 8 characters long, have at least 1 of each case letter, a number and a special character." placeholder="A-Z, a-z, 0-9, specials" required>
+        </div>
+        <div class="form-group">
+          <label for="password" id="confirm-label">Confirm Password</label>
+          <input type="password" class="confirm" name="confirm" placeholder="1 of each, min. 8 chars" required>
+        </div>
+        <div class="button-group">
+          <button type="submit" class="register-button">Confirm</button>
+          <button type="submit" class="cancel-button">Cancel</button>
+        </div>
+      </form>
+    </div>
+  `;
+
+  const usernameInput = document.querySelector(".username");
+  usernameInput.addEventListener("keyup", validateUsername);
+
+  const passwordInput = document.querySelector(".password");
+  passwordInput.addEventListener("keyup", validatePassword);
+  passwordInput.addEventListener("keyup", validateConfirm);
+
+  const confirmInput = document.querySelector(".confirm");
+  confirmInput.addEventListener("keyup", validateConfirm);
+
+  const registerButton = document.querySelector(".register-button");
+  registerButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    registerUser();
+  });
+
+  const cancelButton = document.querySelector(".cancel-button");
+  cancelButton.addEventListener("click", loginContainer);
+  cancelButton.addEventListener("click", loginFooter);
+}
+
+function notesContainer() {
+  const footer = document.querySelector("footer");
+  footer.classList.toggle('footer');
+  footer.innerHTML = "";
+
+  const container = document.querySelector("section");
+  container.innerHTML = `
+    <div class="notes">
+      <div>
+        <h1>notespage for testing</h1>
+      </div>
+    </div>
+  `;
+
+  const usernameInput = document.querySelector(".username");
+  usernameInput.addEventListener("keyup", validateUsername);
+
+  const passwordInput = document.querySelector(".password");
+  passwordInput.addEventListener("keyup", validatePassword);
+  passwordInput.addEventListener("keyup", validateConfirm);
+
+  const confirmInput = document.querySelector(".confirm");
+  confirmInput.addEventListener("keyup", validateConfirm);
+
+  const registerButton = document.querySelector(".register-button");
+  registerButton.addEventListener("click", function(event) {
+    // event.preventDefault();
+    registerUser();
+  });
+
+  const cancelButton = document.querySelector(".cancel-button");
+  cancelButton.addEventListener("click", loginContainer);
+  cancelButton.addEventListener("click", loginFooter);
+}
+
+function loginFooter() {
+  const footer = document.querySelector("footer");
+  footer.classList.toggle('footer');
+  footer.innerHTML = `
+  <div class="footer-content">
+			<a href="https://github.com/n-c0de-r/StegaNotes">
+      <span style="color: var(--mainGreen);">n-c0de-r</span>
+      <span> @ </span>
+      <span style="color: var(--mainRed);">GitHub</span>
+      <span>2023</span>
+    </a>
+	</div>
+  `;
+}
+
+/**
+ * Creates a SVG from a given name.
+ * @param {string} name The username to turn to a SVG
+ * @returns A SVG holding the users name
+ */
+function createSVG(name) {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("width", "1000");
+  svg.setAttribute("height", "210");
+  svg.setAttribute("viewBox", "0 0 1000 210");
+  const size = Math.min(1000/(name.length+2), 100);
+  const offset1 = name.length/4;
+  const offset2 = name.length/8;
+  // TODO Fix the formula
+  const text1 = createText(`${name}'s${" ".repeat(offset1+offset2+4)}`, "#6dca4d", "end", size);
+  const text2 = createText("Notes "+" ".repeat(offset2), "#ed1c24", "end", size);
+
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  group.appendChild(text1);
+  group.appendChild(text2);
+
+  svg.appendChild(group);
+
+  return svg;
+}
+
+/**
+ * Creates a text element to nest inside a SVG.
+ * @param {string} content Text to make an SVG from
+ * @param {string} color The color of the text
+ * @param {string} anchor How to anchor the text
+ * @returns The text as an SVG element
+ */
+function createText(content, color, anchor, size) {
+  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  text.setAttribute("x", "100%"); // 4=55, 6=60, 8=65 9=67.5 10=72,5 11=70 12=67.5 
+  text.setAttribute("y", "50%");
+  text.setAttribute("text-anchor", anchor);
+  text.setAttribute("font-family", "OpenDyslexic3");
+  text.setAttribute("font-size", size);
+  text.setAttribute("fill", color);
+  text.setAttribute("stroke-width", "3.7772");
+  text.setAttribute("stroke", "#000000");
+  text.setAttribute("xml:space", "preserve");
+  text.textContent = content;
+
+  return text;
+}
+
+/**
+ * Serializes a SVG as a Base64 URL to use as a src for an image.
+ * @param {*} svg The SVG to convert
+ * @returns Base64 Data URL
+ */
+function serializeSVG(svg) {
+  const serializer = new XMLSerializer();
+  const svgString = serializer.serializeToString(svg);
+  const dataURL = "data:image/svg+xml;base64," + btoa(svgString);
+
+  return dataURL;
+}
+
+// Login functions //
+
+/**
+ * Logins a User and Opens their database.
+ */
+async function login() {
+  const username = document.querySelector(".username").value;
+  const password = document.querySelector(".password").value;
+  const usedName = await existsDB(username);
+
+  if(username.length>0 && usedName) {
+    const request = indexedDB.open(username, 1);
+
+    request.onsuccess = function() {
+      const db = request.result;
+      const transaction = db.transaction("logins", "readonly");
+      const logins = transaction.objectStore("logins");
+      
+      const login = logins.get(username);
+
+      login.onsuccess = async function() {
+        let creds = await login.result;
+        const user = creds.user;
+        const pass = creds.pass;
+        const salt = creds.salt;
+        const hash = await hashPassword(password, salt);
+
+      if(hash === pass) {
+        loginHeader(username);
+        notesContainer();
       } else {
-        let { password: storedPassword, salt } = user;
-        let hashedPassword = hashPassword(password, salt);
-        if (hashedPassword !== storedPassword) {
-          reject("Incorrect password");
-        } else {
-          resolve("Login successful");
-        }
+        alert(`Entered credentials seem to be wrong!\nTry again.`);
       }
+      };
+      
+      transaction.oncomplete = function () {
+        db.close();
+      };
     };
-
-    request.onerror = (event) => {
-      reject(event.target.error);
-    };
-  });
+  }
 }
 
-function openDatabase() {
-  return new Promise((resolve, reject) => {
-    let request = indexedDB.open("myDatabase", 1);
-    request.onerror = (event) => {
-      console.error("Database error:", event.target.errorCode);
-      reject(event.target.errorCode);
-    };
-    request.onsuccess = (event) => {
-      let db = event.target.result;
-      console.log("Database opened successfully");
-      resolve(db);
-    };
-    request.onupgradeneeded = (event) => {
-      let db = event.target.result;
-      let store = db.createObjectStore("users", {
-        keyPath: "username",
-      });
-      store.createIndex("username", "username", { unique: true });
-      console.log("Database upgraded successfully");
-    };
-  });
+/**
+ * Registers a new User and creates a new database with their name.
+ */
+function registerUser() {
+  if (isValidUsername && isValidPassword && isConfirmed) {
+    const username = document.querySelector(".username").value;
+    const password = document.querySelector(".password").value;
+    openDatabase(username, password);
+  }
 }
 
+/**
+ * Validates if a username is already used for a database.
+ */
+async function validateUsername() {
+  const minLength = 4;
+  const username = document.querySelector(".username").value;
+  const userLabel = document.querySelector("#username-label");
+
+  if (username.trim() === "") {
+    userLabel.innerHTML = 'New Username';
+    return;
+  }
+  const usedName = await existsDB(username);
+
+  if (!usedName && username.length >= minLength) {
+    userLabel.innerHTML = 'New Username' + '✔️';
+  } else {
+    userLabel.innerHTML = 'New Username' + '❌';
+  }
+
+  isValidUsername = !usedName && username.length >= minLength;
+}
+
+/**
+ * Validates the structure of a given password.
+ * Passwords must have both cases, special characters, 
+ * numbers and be at least 8 chars long.
+ */
+function validatePassword() {
+  const password = document.querySelector(".password").value;
+  const passLabel = document.querySelector("#password-label");
+
+  if (password.trim() === "") {
+    passLabel.innerHTML = 'New Password';
+    return;
+  }
+
+  const minLength = 8;
+  const hasSpecialChar = /[!@#$%^&*(),.?:{}|<>]/.test(password);
+  const hasUpperCase = /[A-Z]/g.test(password);
+  const hasLowerCase = /[a-z]/g.test(password);
+  const hasNumber = /\d/g.test(password);
+
+  const validPassword = password.length >= minLength &&
+    hasSpecialChar &&
+    hasUpperCase &&
+    hasLowerCase &&
+    hasNumber;
+
+  if (validPassword) {
+    passLabel.innerHTML = 'New Password' + '✔️';
+  } else {
+    passLabel.innerHTML = 'New Password' + '❌';
+  }
+
+  isValidPassword = validPassword;
+}
+
+/**
+ * Validates if the retype of a pasword is correct.
+ */
+function validateConfirm() {
+  const password = document.querySelector(".password").value;
+  const confirm = document.querySelector(".confirm").value;
+  const confLabel = document.querySelector("#confirm-label");
+
+  if (confirm.trim() === "") {
+    confLabel.innerHTML = 'Confirm Password';
+    return;
+  }
+
+  if (password === confirm) {
+    confLabel.innerHTML = 'Confirm Password' + '✔️';
+  } else {
+    confLabel.innerHTML = 'Confirm Password' + '❌';
+  }
+
+  isConfirmed = password === confirm;
+}
+
+/**
+ * Checks if a database exsists.
+ * @param {string} name DB name to check.
+ * @returns True if the DB exists.
+ */
+async function existsDB(name) {
+  let validName = false;
+
+  const dbs = await indexedDB.databases();
+  dbs.forEach(db => {
+    validName = validName || db.name === name;
+  });
+
+  return validName;
+}
+
+// Database functions //
+
+/**
+ * Creates a new database with the user's name
+ * and stores the Password in a new storage.
+ * @param {string} username Database name.
+ * @param {string} password User password.
+ */
+async function openDatabase(username, password) {
+  if (!window.indexedDB) {
+    alert('IndexedDB not supported');
+    return;
+  }
+
+  const saltHash = generateSalt();
+  const passHash = await hashPassword(password, saltHash);
+  
+  const request = indexedDB.open(username, 1);
+
+  request.onupgradeneeded = function() {
+    const db = request.result;
+    if (!db.objectStoreNames.contains('logins')) {
+      db.createObjectStore("logins", {keyPath: 'user'});
+      db.createObjectStore("notes", {keyPath: 'title'});
+    }
+  };
+  
+  request.onsuccess = function() {
+    const db = request.result;
+    const transaction = db.transaction("logins", "readwrite");
+    const logins = transaction.objectStore("logins");
+    
+    const login = {user: username, pass: passHash, salt: saltHash};
+    logins.add(login);
+    
+    transaction.oncomplete = function () {
+      alert(`User \"${username}\" successfully created.\n
+      Please be sure to remember your password.\n
+      There's no way to retrieve it for now!`);
+      db.close();
+      loginContainer();
+      loginFooter();
+    };
+  };
+}
+
+function addNote() {
+  // TODO: add function
+}
+
+// Crypto functions //
+
+/**
+ * Hashes a salted password string.
+ * @param {string} password Original password 
+ * @param {string} salt Generated salt.
+ * @returns Hash of salted password
+ */
 async function hashPassword(password, salt) {
-  let encoder = new TextEncoder();
-  let data = encoder.encode(password + salt);
-  let hash = await crypto.subtle.digest("SHA-256", data);
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password + salt);
+  const hash = await crypto.subtle.digest("SHA-256", data);
   return hexString(hash);
 }
 
-function hexString(buffer) {
-  let byteArray = new Uint8Array(buffer);
-  let hexCodes = [...byteArray].map((value) => {
-    let hexCode = value.toString(16);
-    let paddedHexCode = hexCode.padStart(2, "0");
-    return paddedHexCode;
-  });
-  return hexCodes.join("");
-}
-
+/**
+ * Generates a hexadecimal salt for passwords.
+ * @returns Returns a hexadecimal salt string.
+ */
 function generateSalt() {
-  let buffer = new Uint8Array(16);
+  const buffer = new Uint8Array(64);
   crypto.getRandomValues(buffer);
   return hexString(buffer);
 }
 
-let loginButton = document.querySelector("#login-button");
-loginButton.addEventListener("click", async () => {
-  let username = document.querySelector("#username").value;
-  let password = document.querySelector("#password").value;
-  try {
-    let result = await login(username, password);
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-    let modal = error instanceof DOMException ? createModal(error.message, []) : error;
-    modal.show();
-  }
-});
+/**
+ * Converts a buffer array to hexstring.
+ * @param {Uint8Array} buffer The input buffer.
+ * @returns Hexadecimal of the buffer.
+ */
+function hexString(buffer) {
+  const byteArray = new Uint8Array(buffer);
+  const hexCodes = [...byteArray].map((value) => {
+    const hexCode = value.toString(16);
+    const paddedHexCode = hexCode.padStart(2, "0");
+    return paddedHexCode;
+  });
+  return hexCodes.join("");
+}
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "cache1"; // Change value to force update
+const cacheName = "stegaNotesCache"; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
@@ -8,42 +8,57 @@ self.addEventListener("install", event => {
 		caches.open(cacheName).then(cache => {
 			return cache.addAll([
 				"/",
-				"android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
-				"android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
-				"android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
-				"android-chrome-96x96.png", // Favicon, Android Chrome M39+ with 2.0 screen density
-				"android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 3.0 screen density
-				"android-chrome-192x192.png", // Favicon, Android Chrome M39+ with 4.0 screen density
-				"android-chrome-256x256.png", // Favicon, Android Chrome M47+ Splash screen with 1.5 screen density
-				"android-chrome-384x384.png", // Favicon, Android Chrome M47+ Splash screen with 3.0 screen density
-				"android-chrome-512x512.png", // Favicon, Android Chrome M47+ Splash screen with 4.0 screen density
-				"apple-touch-icon.png", // Favicon, Apple default
-				"apple-touch-icon-57x57.png", // Apple iPhone, Non-retina with iOS6 or prior
-				"apple-touch-icon-60x60.png", // Apple iPhone, Non-retina with iOS7
-				"apple-touch-icon-72x72.png", // Apple iPad, Non-retina with iOS6 or prior
-				"apple-touch-icon-76x76.png", // Apple iPad, Non-retina with iOS7
-				"apple-touch-icon-114x114.png", // Apple iPhone, Retina with iOS6 or prior
-				"apple-touch-icon-120x120.png", // Apple iPhone, Retina with iOS7
-				"apple-touch-icon-144x144.png", // Apple iPad, Retina with iOS6 or prior
-				"apple-touch-icon-152x152.png", // Apple iPad, Retina with iOS7
-				"apple-touch-icon-180x180.png", // Apple iPhone 6 Plus with iOS8
-				"browserconfig.xml", // IE11 icon configuration file
-				"favicon.ico", // Favicon, IE and fallback for other browsers
-				"favicon-16x16.png", // Favicon, default
-				"favicon-32x32.png", // Favicon, Safari on Mac OS
 				"index.html", // Main HTML file
-				"logo.png", // Logo
-				"main.js", // Main Javascript file
+				"script.js", // Main Javascript file
 				"manifest.json", // Manifest file
-				"maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
-				"mstile-70x70.png", // Favicon, Windows 8 / IE11
-				"mstile-144x144.png", // Favicon, Windows 8 / IE10
-				"mstile-150x150.png", // Favicon, Windows 8 / IE11
-				"mstile-310x150.png", // Favicon, Windows 8 / IE11
-				"mstile-310x310.png", // Favicon, Windows 8 / IE11
-				"safari-pinned-tab.svg", // Favicon, Safari pinned tab
-				"share.jpg", // Social media sharing
 				"style.css", // Main CSS file
+				"/fonts/OpenDyslexic3-Regular.ttf", // Font
+				"/fonts/OpenDyslexic-Regular.otf", // Font
+				"/fonts/OpenDyslexicMono-Regular.otf", // Font
+				"/icons/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
+				"/icons/android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
+				"/icons/android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
+				"/icons/android-chrome-96x96.png", // Favicon, Android Chrome M39+ with 2.0 screen density
+				"/icons/android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 3.0 screen density
+				"/icons/android-chrome-192x192.png", // Favicon, Android Chrome M39+ with 4.0 screen density
+				"/icons/android-chrome-256x256.png", // Favicon, Android Chrome M47+ Splash screen with 1.5 screen density
+				"/icons/android-chrome-384x384.png", // Favicon, Android Chrome M47+ Splash screen with 3.0 screen density
+				"/icons/android-chrome-512x512.png", // Favicon, Android Chrome M47+ Splash screen with 4.0 screen density
+				"/icons/apple-touch-icon.png", // Favicon, Apple default
+				"/icons/apple-touch-icon-57x57.png", // Apple iPhone, Non-retina with iOS6 or prior
+				"/icons/apple-touch-icon-60x60.png", // Apple iPhone, Non-retina with iOS7
+				"/icons/apple-touch-icon-72x72.png", // Apple iPad, Non-retina with iOS6 or prior
+				"/icons/apple-touch-icon-76x76.png", // Apple iPad, Non-retina with iOS7
+				"/icons/apple-touch-icon-114x114.png", // Apple iPhone, Retina with iOS6 or prior
+				"/icons/apple-touch-icon-120x120.png", // Apple iPhone, Retina with iOS7
+				"/icons/apple-touch-icon-144x144.png", // Apple iPad, Retina with iOS6 or prior
+				"/icons/apple-touch-icon-152x152.png", // Apple iPad, Retina with iOS7
+				"/icons/apple-touch-icon-180x180.png", // Apple iPhone 6 Plus with iOS8
+				"/icons/browserconfig.xml", // IE11 icon configuration file
+				"/icons/favicon.ico", // Favicon, IE and fallback for other browsers
+				"/icons/favicon.png", // Favicon, IE and fallback for other browsers
+				"/icons/favicon-16x16.png", // Favicon, default
+				"/icons/favicon-32x32.png", // Favicon, Safari on Mac OS
+				"/icons/icon.svg", // icon as a svg, it's smaller and scalable
+				"/icons/logo.png", // full app logo
+				"/icons/logo.svg", // full app logo as a svg, it's smaller and scalable
+				"/icons/name.png", // app name logo,
+				"/icons/name.svg", // app name logo as a svg, it's smaller and scalable
+				"/icons/maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
+				"/icons/maskable_icon_x48.png", // Favicon, maskable
+				"/icons/maskable_icon_x72.png", // Favicon, maskable
+				"/icons/maskable_icon_x96.png", // Favicon, maskable
+				"/icons/maskable_icon_x128.png", // Favicon, maskable
+				"/icons/maskable_icon_x192.png", // Favicon, maskable
+				"/icons/maskable_icon_x384.png", // Favicon, maskable
+				"/icons/maskable_icon_x512.png", // Favicon, maskable
+				"/icons/mstile-70x70.png", // Favicon, Windows 8 / IE11
+				"/icons/mstile-144x144.png", // Favicon, Windows 8 / IE10
+				"/icons/mstile-150x150.png", // Favicon, Windows 8 / IE11
+				"/icons/mstile-310x150.png", // Favicon, Windows 8 / IE11
+				"/icons/mstile-310x310.png", // Favicon, Windows 8 / IE11
+				"/icons/safari-pinned-tab.svg", // Favicon, Safari pinned tab
+				"/icons/share.jpg", // Social media sharing
 			]);
 		})
 	);
